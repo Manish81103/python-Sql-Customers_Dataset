f077119f6040e9cc46a1d21c7e486861ee04100e
Manish <manishsingh832003z@gmail.com>
2024-10-18 07:31:21 +0530

    first commit

---
+++ dataset.sql
@@ -0,0 +1,9 @@
+create database ecommerce;
+use ecommerce;
+select * from customers;
+select * from orders;
+select * from geolocation;
+select * from order_items;
+select * from products;
+select * from sellers;
+select * from payments;
